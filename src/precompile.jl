5912c3bba414e26f494288ea92c85810b8146494
Chris Elrod <elrodc@gmail.com>
2019-12-02 17:55:01 -0500

    Removed kwvars from precompile.jl

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,8 @@
+
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     precompile(Tuple{typeof(VectorizedRNG.random_init_pcg!),VectorizedRNG.PtrPCG{4},Int64})
     precompile(Tuple{typeof(VectorizedRNG.rand_pcg_float_quote),Int64,Int64,Int64,Type{Float64},VectorizedRNG.PCG_Algorithm,Symbol,Symbol})
     precompile(Tuple{typeof(VectorizedRNG.randn_quote),Int64,Int64,Int64,Type,VectorizedRNG.PCG_Algorithm})
-    precompile(Tuple{VectorizedRNG.var"#rand_pcg_float_quote##kw",NamedTuple{(:uload,),Tuple{Bool}},typeof(VectorizedRNG.rand_pcg_float_quote),Int64,Int64,Int64,Type{Float64},VectorizedRNG.PCG_Algorithm})
+    # precompile(Tuple{VectorizedRNG.var"#rand_pcg_float_quote##kw",NamedTuple{(:uload,),Tuple{Bool}},typeof(VectorizedRNG.rand_pcg_float_quote),Int64,Int64,Int64,Type{Float64},VectorizedRNG.PCG_Algorithm})
 end
